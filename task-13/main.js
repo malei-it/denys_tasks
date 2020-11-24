adfbf460daf9e84c91e6c793c2b3ed31fa846011
user <user@users-MacBook-Pro.local>
2020-11-24 08:22:17 +0200

    done task 13

--- task-13/main.js
+++ task-13/main.js
@@ -1,10 +1,9 @@
 function square () {
     var b= [];
     for (i=0; i<10; i++) {
-        b += i**2;
+        b[i] = i**2;
     }
-    console.log (b); 
-    //return b;  
+    return b;  
 }
 var a = square();
 console.log(a);
