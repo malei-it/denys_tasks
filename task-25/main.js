4e3dbb249005ef983c5099d86f48432ca078258a
user <user@users-MacBook-Pro.local>
2020-12-01 20:15:07 +0200

    done homework part3

---
+++ task-25/main.js
@@ -0,0 +1,10 @@
+
+function checkingNum(num1, num2) {
+    if (((num1>3)&(num1<10))||((num2>=7)&(num2<12))) { 
+        alert('верно');
+    }
+    else {
+        alert('Неверно');
+    }
+}
+checkingNum(6,4);
---
+++ task-26/main.js
@@ -0,0 +1,11 @@
+
+function numOperation(num1) {
+    if ((num1==2)||(num1==5)){
+        num1=num1+3;
+    }
+    else {
+        num1=num1/10;
+    }
+    alert(num1);
+}
+numOperation(5);
