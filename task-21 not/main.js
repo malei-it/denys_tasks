4d4701440e551090500b7a644fae3a32aed620ce
user <user@users-MacBook-Pro.local>
2020-12-01 21:38:14 +0200

    full done homework

--- task-21 not/main.js
+++ task-21 not/main.js
@@ -9,5 +9,12 @@ function arrayCreation1() {
 }
 console.log(arrayCreation1());
 
+function arrayCreation2(n){
+    var a=[]
+    for (i=0;i<n;i++){
+        a[i]=i+1;
+    }
+    return a;
+}
 
-
+console.log(arrayCreation2(5));
